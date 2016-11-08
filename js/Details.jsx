92fbca24345eb9bc572f604ee866991ba50fb4fd
Cy Weisman <cyweisman@gmail.com>
2016-11-08 13:34:51 -0600

    add animation card to details, still needs work

--- js/Details.jsx
+++ js/Details.jsx
@@ -3,15 +3,60 @@ const React = require('react')
 class Details extends React.Component {
   render () {
     const params = this.props.params || {}
-    const { name, photo, music, special_moment } = params
+    const { name, photo, music, activity, special_moment, last_visit } = params
     return (
       <div className='container'>
-        <div className='person-info'>
-          <h1 className='person-title'>{name}</h1>
-          <h2 className='person-year'>{music}</h2>
-          <img className='person-poster' src={`public/img/posters/${photo}`} />
-          <p className='person-description'>{special_moment}</p>
+
+        <div className="detail-photo">
+          <img src={`public/img/posters/${photo}`} />
         </div>
+
+        <div className='flip-container' >
+            <div className='flipper'>
+              <div className='front'>
+                <img src="https://pbs.twimg.com/profile_images/761645430937522177/f5-8JkIE.jpg" alt="Smiley face" height="390" width="390" />
+              </div>
+              <div className='back'>
+                <p>{music}</p>
+              </div>
+            </div>
+        </div>
+
+        <div className='flip-container' >
+            <div className='flipper'>
+              <div className='front'>
+                <img src="https://pbs.twimg.com/profile_images/761645430937522177/f5-8JkIE.jpg" alt="Smiley face" height="390" width="390" />
+              </div>
+              <div className='back'>
+                <p>{activity}</p>
+              </div>
+            </div>
+        </div>
+
+        <div className='flip-container' >
+            <div className='flipper'>
+              <div className='front'>
+                <img src="https://pbs.twimg.com/profile_images/761645430937522177/f5-8JkIE.jpg" alt="Smiley face" height="390" width="390" />
+              </div>
+              <div className='back'>
+                <p>{special_moment}</p>
+              </div>
+            </div>
+        </div>
+
+        <div className='flip-container' >
+            <div className='flipper'>
+              <div className='front'>
+                <img src="https://pbs.twimg.com/profile_images/761645430937522177/f5-8JkIE.jpg" alt="Smiley face" height="390" width="390" />
+              </div>
+              <div className='back'>
+                <p>{last_visit}</p>
+              </div>
+            </div>
+        </div>
+
+
+
       </div>
     )
   }
