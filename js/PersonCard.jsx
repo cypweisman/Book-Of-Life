df7b716e3d0635d2ab75cfc393824f295869e46b
Cy Weisman <cyweisman@gmail.com>
2016-11-10 12:50:31 -0600

    styling for main

--- js/PersonCard.jsx
+++ js/PersonCard.jsx
@@ -4,7 +4,7 @@ const { Link } = require('react-router')
 const PersonCard = (props) => (
   <Link to={`/details/${props.person_id}`} className='person-card'>
     <div>
-      <img src={`public/img/posters/${props.photo}`} className='person-card-img' />
+        <img src={`public/img/posters/${props.photo}`} className='person-card-img' />
       <div className='person-card-text'>
         <h3 className='person-card-title'>{props.name}</h3>
       </div>
